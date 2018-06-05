1bfd1a110e9aae707b6e17dd03cc03aae9276c6b
Jeka Lion <djoni-01@outlook.com>
2018-06-05 22:58:17 +0600

    init

---
+++ ChatServerApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApp
+{
+    class Program
+    {
+        private const int SERVER_PORT = 3535;
+        private static List<Socket> sockets;
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), SERVER_PORT);
+            serverSocket.Bind(endPoint);
+
+            Server server = new Server();
+            server.Work(serverSocket);
+            
+        }
+    }
+}
---
+++ ChatServerApp/Server.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServerApp
+{
+    public class Server
+    {
+        private const int MAX_CLIENT_QUEUE = 3;
+        private List<Socket> sockets = new List<Socket>();
+        
+
+        public void Work(Socket serverSocket)
+        {
+            try
+            {
+                Console.WriteLine("Сервер запущен...");
+                serverSocket.Listen(MAX_CLIENT_QUEUE);
+                while (true)
+                {
+                    //-------Помещаем сокет в массив
+                    sockets.Add(serverSocket.Accept());
+                    int socketIndex = sockets.Count - 1;
+                    //-------
+                    ConnectClient(socketIndex);
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                serverSocket.Close();
+                for (int i = 0; i < sockets.Count; i++)
+                {
+                    sockets[i].Shutdown(SocketShutdown.Both);
+                }
+                Console.WriteLine("Сервер завершил свою работу...");
+            }
+        }
+
+        public Task ConnectClient(int socketIndex)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        //-------Принимаем данные с сокета
+                        int bytes;
+                        byte[] buffer = new byte[1024];
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        do
+                        {
+                            bytes = sockets[socketIndex].Receive(buffer);
+                            stringBuilder.Append(Encoding.Default.GetString(buffer));
+                        }
+                        while (sockets[socketIndex].Available > 0);
+
+                        UserMessage newMessage = JsonConvert.DeserializeObject<UserMessage>(stringBuilder.ToString());
+                        //-------
+                        //-------Посылаем необходимое сообщение
+                        if (newMessage.Message == "init")
+                        {
+                            Console.WriteLine("В чат вошел... " + newMessage.UserName);
+                            for (int i = 0; i < sockets.Count; i++)
+                            {
+                                if (i != socketIndex)
+                                {
+                                    newMessage.Message = "Вошел в чат!";
+                                    string serialized = JsonConvert.SerializeObject(newMessage);
+                                    sockets[i].Send(Encoding.Default.GetBytes(serialized));
+                                }
+                            }
+                        }
+                        else if (newMessage.Message == "exit")
+                        {
+                            Console.WriteLine("Из чата вышел... " + newMessage.UserName);
+                            for (int i = 0; i < sockets.Count; i++)
+                            {
+                                if (i != socketIndex)
+                                {
+                                    newMessage.Message = "Покинул чат!";
+                                    string serialized = JsonConvert.SerializeObject(newMessage);
+                                    sockets[i].Send(Encoding.Default.GetBytes(serialized));
+                                }
+                            }
+                            sockets[socketIndex].Shutdown(SocketShutdown.Both);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Сообщение отправил... " + newMessage.UserName);
+                            for (int i = 0; i < sockets.Count; i++)
+                            {
+                                if (i != socketIndex)
+                                {
+                                    string serialized = JsonConvert.SerializeObject(newMessage);
+                                    sockets[i].Send(Encoding.Default.GetBytes(serialized));
+                                }
+                            }
+                        }
+                        //-------
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+    }
+}
